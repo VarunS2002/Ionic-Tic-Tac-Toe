5301bf6252aae659356a0ea88c44ab68e93a0878
VarunS2002 <30069380+VarunS2002@users.noreply.github.com>
2020-10-09 10:28:30 +0530

    - Changed 1D array to 2D array - Added rxjs-compat - Updated iPhone splash - Replaced deprecated HTML attributes with CSS

--- tic-tac-toe/src/app/home/home.page.ts
+++ tic-tac-toe/src/app/home/home.page.ts
@@ -7,29 +7,32 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
   f: number;
-  data: string[];
+  data: [string[], string[], string[]];
   gameOver: boolean;
+  ids: string[];
 
   constructor() {
     this.f = 0;
-    this.data = ['', '', '', '', '', '', '', '', ''];
+    this.data = [['', '', ''],
+                 ['', '', ''],
+                 ['', '', '']];
     this.gameOver = false;
   }
 
-  callback(id: number) {
-    if (this.gameOver){
+  callback(id: string) {
+    if (this.gameOver) {
       return;
     }
     if (this.f === 0) {
-      if (document.getElementById(id.toString()).innerHTML === '') {
-        document.getElementById(id.toString()).innerHTML = 'X';
-        this.data[id] = 'X';
+      if (document.getElementById(id).innerHTML === '') {
+        document.getElementById(id).innerHTML = 'X';
+        this.data[Number((id.substr(0,1)))][Number(id.substr(1,2))] = 'X';
         this.f += 1;
       }
     } else if (this.f === 1) {
-      if (document.getElementById(id.toString()).innerHTML === '') {
-        document.getElementById(id.toString()).innerHTML = 'O';
-        this.data[id] = 'O';
+      if (document.getElementById(id).innerHTML === '') {
+        document.getElementById(id).innerHTML = 'O';
+        this.data[Number((id.substr(0,1)))][Number(id.substr(1,2))] = 'O';
         this.f -= 1;
       }
     }
@@ -37,14 +40,13 @@ export class HomePage {
   }
 
   result(winner: boolean = true) {
-    if (winner){
+    if (winner) {
       if (this.f === 0) {
         this.alert_O();
       } else if (this.f === 1) {
         this.alert_X();
       }
-    }
-    else {
+    } else {
       this.alert_draw();
     }
     this.gameOver = true;
@@ -52,10 +54,12 @@ export class HomePage {
 
   new_game() {
     this.f = 0;
-    this.data = ['', '', '', '', '', '', '', '', ''];
+    this.data = [['', '', ''], ['', '', ''], ['', '', '']];
     this.gameOver = false;
-    for (let i = 0; i < 9; i++) {
-      document.getElementById(i.toString()).innerHTML = '';
+    for (let x = 0; x < 3; x++) {
+      for (let y = 0; y < 3; y++) {
+        document.getElementById(String(x) + String(y)).innerHTML = '';
+      }
     }
   }
 
@@ -80,6 +84,7 @@ export class HomePage {
     document.body.appendChild(alert);
     return alert.present();
   }
+
   alert_X() {
     const alert = document.createElement('ion-alert');
     alert.cssClass = 'my-custom-class';
@@ -91,104 +96,109 @@ export class HomePage {
     return alert.present();
   }
 
-  check(id: number) {
-    if (this.gameOver){
+  check(id: string) {
+    if (this.gameOver) {
       return;
     }
-    if (id === 0) {
-      if (this.data[0] === this.data[1] && this.data[1] === this.data[2]) {
+    if (id === '00') {
+      if (this.data[0][0] === this.data[0][1] && this.data[0][1] === this.data[0][2]) {
         this.result();
         return;
-      } else if (this.data[0] === this.data[3] && this.data[3] === this.data[6]) {
+      } else if (this.data[0][0] === this.data[1][0] && this.data[1][0] === this.data[2][0]) {
         this.result();
         return;
-      } else if (this.data[0] === this.data[4] && this.data[4] === this.data[8]) {
-        this.result();
-        return;
-      }
-    } else if (id === 1) {
-      if (this.data[0] === this.data[1] && this.data[1] === this.data[2]) {
-        this.result();
-        return;
-      } else if (this.data[1] === this.data[4] && this.data[4] === this.data[7]) {
+      } else if (this.data[0][0] === this.data[1][1] && this.data[1][1] === this.data[2][2]) {
         this.result();
         return;
       }
-    } else if (id === 2) {
-      if (this.data[0] === this.data[1] && this.data[1] === this.data[2]) {
+    } else if (id === '01') {
+      if (this.data[0][1] === this.data[1][1] && this.data[1][1] === this.data[2][1]) {
         this.result();
         return;
-      } else if (this.data[2] === this.data[5] && this.data[5] === this.data[8]) {
-        this.result();
-        return;
-      } else if (this.data[2] === this.data[4] && this.data[4] === this.data[6]) {
+      } else if (this.data[0][0] === this.data[0][1] && this.data[0][1] === this.data[0][2]) {
         this.result();
         return;
       }
-    } else if (id === 3) {
-      if (this.data[0] === this.data[3] && this.data[3] === this.data[6]) {
+    } else if (id === '02') {
+      if (this.data[0][0] === this.data[0][1] && this.data[0][1] === this.data[0][2]) {
         this.result();
         return;
-      } else if (this.data[3] === this.data[4] && this.data[4] === this.data[5]) {
+      } else if (this.data[0][2] === this.data[1][2] && this.data[1][2] === this.data[2][2]) {
+        this.result();
+        return;
+      } else if (this.data[0][2] === this.data[1][1] && this.data[1][1] === this.data[2][0]) {
         this.result();
         return;
       }
-    } else if (id === 4) {
-      if (this.data[1] === this.data[4] && this.data[4] === this.data[7]) {
+    } else if (id === '10') {
+      if (this.data[0][0] === this.data[1][0] && this.data[1][0] === this.data[2][0]) {
         this.result();
         return;
-      } else if (this.data[3] === this.data[4] && this.data[4] === this.data[5]) {
+      } else if (this.data[1][0] === this.data[1][1] && this.data[1][1] === this.data[1][2]) {
         this.result();
         return;
-      } else if (this.data[0] === this.data[4] && this.data[4] === this.data[8]) {
+      }
+    } else if (id === '11') {
+      if (this.data[0][0] === this.data[1][1] && this.data[1][1] === this.data[2][2]) {
         this.result();
         return;
-      } else if (this.data[2] === this.data[4] && this.data[4] === this.data[6]) {
+      } else if (this.data[0][1] === this.data[1][1] && this.data[1][1] === this.data[2][1]) {
+        this.result();
+        return;
+      } else if (this.data[0][2] === this.data[1][1] && this.data[1][1] === this.data[2][0]) {
+        this.result();
+        return;
+      } else if (this.data[1][0] === this.data[1][1] && this.data[1][1] === this.data[1][2]) {
         this.result();
         return;
       }
 
-    } else if (id === 5) {
-      if (this.data[2] === this.data[5] && this.data[5] === this.data[8]) {
+    } else if (id === '12') {
+      if (this.data[0][2] === this.data[1][2] && this.data[1][2] === this.data[2][2]) {
         this.result();
         return;
-      } else if (this.data[3] === this.data[4] && this.data[4] === this.data[5]) {
-        this.result();
-        return;
-      }
-    } else if (id === 6) {
-      if (this.data[0] === this.data[3] && this.data[3] === this.data[6]) {
-        this.result();
-        return;
-      } else if (this.data[2] === this.data[4] && this.data[4] === this.data[6]) {
-        this.result();
-        return;
-      } else if (this.data[6] === this.data[7] && this.data[7] === this.data[8]) {
+      } else if (this.data[1][0] === this.data[1][1] && this.data[1][1] === this.data[1][2]) {
         this.result();
         return;
       }
-    } else if (id === 7) {
-      if (this.data[6] === this.data[7] && this.data[7] === this.data[8]) {
+    } else if (id === '20') {
+      if (this.data[0][0] === this.data[1][0] && this.data[1][0] === this.data[2][0]) {
         this.result();
         return;
-      } else if (this.data[1] === this.data[4] && this.data[4] === this.data[7]) {
+      } else if (this.data[0][2] === this.data[1][1] && this.data[1][1] === this.data[2][0]) {
+        this.result();
+        return;
+      } else if (this.data[2][0] === this.data[2][1] && this.data[2][1] === this.data[2][2]) {
         this.result();
         return;
       }
-    } else if (id === 8) {
-      if (this.data[2] === this.data[5] && this.data[5] === this.data[8]) {
+    } else if (id === '21') {
+      if (this.data[2][0] === this.data[2][1] && this.data[2][1] === this.data[2][2]) {
         this.result();
         return;
-      } else if (this.data[0] === this.data[4] && this.data[4] === this.data[8]) {
+      } else if (this.data[0][1] === this.data[1][1] && this.data[1][1] === this.data[2][1]) {
         this.result();
         return;
-      } else if (this.data[6] === this.data[7] && this.data[7] === this.data[8]) {
+      }
+    } else if (id === '22') {
+      if (this.data[2][0] === this.data[2][1] && this.data[2][1] === this.data[2][2]) {
+        this.result();
+        return;
+      } else if (this.data[0][0] === this.data[1][1] && this.data[1][1] === this.data[2][2]) {
+        this.result();
+        return;
+      } else if (this.data[0][2] === this.data[1][2] && this.data[1][2] === this.data[2][2]) {
         this.result();
         return;
       }
     }
-    if (this.data.every(element => element !== '') === true) {
-      this.result(false);
+    for (let x = 0; x < 3; x++) {
+      for (let y = 0; y < 3; y++) {
+        if (this.data[x][y] === '') {
+            return;
+        }
+      }
     }
+    this.result(false);
   }
 }
